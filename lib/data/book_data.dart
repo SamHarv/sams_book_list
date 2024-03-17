f2aa4d2368920f829485446f4e16f90a320b34c6
SamHarv <srharvey93@gmail.com>
2024-03-17 11:02:34 +1100

    First three books added

--- lib/data/book_data.dart
+++ lib/data/book_data.dart
@@ -2,75 +2,30 @@ import '/models/book_model.dart';
 
 List<Book> books = [
   Book(
-    title: 'Limitless: Upgrade Your Brain, Learn Anything Faster, and Unlock '
-        'Your Exceptional Life',
-    author: 'Jim Kwik',
-    coverImage: 'images/limitless.jpg',
-    rating: 5,
-    rank: '1',
-    url: 'https://www.amazon.com.au/Limitless-Jim-Kwik/dp/1401958230',
-  ),
-  Book(
     title: 'Essentialism: The Disciplined Pursuit of Less',
     author: 'Greg McKeown',
     coverImage: 'images/essentialism.jpg',
-    rating: 8,
+    rating: 10,
+    rank: '1',
+    url:
+        'https://www.amazon.com/Essentialism-Disciplined-Pursuit-Greg-McKeown/dp/0804137382',
+  ),
+  Book(
+    title: 'Think Again: The Power of Knowing What You Don\'t Know',
+    author: 'Adam Grant',
+    coverImage: 'images/think_again.jpg',
+    rating: 10,
     rank: '2',
     url:
-        'https://www.amazon.com/Essentialism-Disciplined-Pursuit-Greg-McKeown/dp/0804137382',
+        'https://www.amazon.com.au/Think-Again-Power-Knowing-What/dp/1984878107',
   ),
   Book(
     title: 'Limitless: Upgrade Your Brain, Learn Anything Faster, and Unlock '
         'Your Exceptional Life',
     author: 'Jim Kwik',
     coverImage: 'images/limitless.jpg',
-    rating: 5,
-    rank: '88',
+    rating: 10,
+    rank: '3',
     url: 'https://www.amazon.com.au/Limitless-Jim-Kwik/dp/1401958230',
-  ),
-  Book(
-    title: 'Essentialism: The Disciplined Pursuit of Less',
-    author: 'Greg McKeown',
-    coverImage: 'images/essentialism.jpg',
-    rating: 8,
-    rank: '100',
-    url:
-        'https://www.amazon.com/Essentialism-Disciplined-Pursuit-Greg-McKeown/dp/0804137382',
-  ),
-  Book(
-    title: 'Limitless: Upgrade Your Brain, Learn Anything Faster, and Unlock '
-        'Your Exceptional Life',
-    author: 'Jim Kwik',
-    coverImage: 'images/limitless.jpg',
-    rating: 5,
-    rank: '1',
-    url: 'https://www.amazon.com.au/Limitless-Jim-Kwik/dp/1401958230',
-  ),
-  Book(
-    title: 'Essentialism: The Disciplined Pursuit of Less',
-    author: 'Greg McKeown',
-    coverImage: 'images/essentialism.jpg',
-    rating: 8,
-    rank: '2',
-    url:
-        'https://www.amazon.com/Essentialism-Disciplined-Pursuit-Greg-McKeown/dp/0804137382',
-  ),
-  Book(
-    title: 'Limitless: Upgrade Your Brain, Learn Anything Faster, and Unlock '
-        'Your Exceptional Life',
-    author: 'Jim Kwik',
-    coverImage: 'images/limitless.jpg',
-    rating: 5,
-    rank: '1',
-    url: 'https://www.amazon.com.au/Limitless-Jim-Kwik/dp/1401958230',
-  ),
-  Book(
-    title: 'Essentialism: The Disciplined Pursuit of Less',
-    author: 'Greg McKeown',
-    coverImage: 'images/essentialism.jpg',
-    rating: 8,
-    rank: '2',
-    url:
-        'https://www.amazon.com/Essentialism-Disciplined-Pursuit-Greg-McKeown/dp/0804137382',
   ),
 ];
